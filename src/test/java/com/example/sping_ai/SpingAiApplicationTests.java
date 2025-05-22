98815be736e96d49042364adfc2cabf2eb932ef2
Andres <andresgouts@gmail.com>
2025-05-22 12:00:46 -0500

    playing with spring ai

---
+++ src/test/java/com/example/sping_ai/SpingAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sping_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
